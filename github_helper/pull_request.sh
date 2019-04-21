3433182739ea36fd57820dd96cd013ee436820d8
tydlwav <michael.mao99@gmail.com>
2019-04-21 01:49:19 -0400

    update pr

--- github_helper/pull_request.sh
+++ github_helper/pull_request.sh
@@ -6,7 +6,7 @@ git checkout $BRANCH
 git merge origin master
 if [ ! "$1" = "--no-edit" ];
 then
-  hub pull-request -m "$BRANCH: " -b master -e
+  hub pull-request -m "$BRANCH: $1" -b master -e
 else
   hub pull-request -b master --no-edit
 fi
