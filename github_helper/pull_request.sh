96a2e91bb537b336320b438946182eed96f51daf
tydlwav <michael.mao99@gmail.com>
2019-04-25 23:12:30 -0400

    add default to pr

--- github_helper/pull_request.sh
+++ github_helper/pull_request.sh
@@ -4,6 +4,13 @@ git checkout master
 git pull
 git checkout $BRANCH
 git merge origin master
+
+if [ -z "$1" ];
+then
+  hub pull-request
+  exit
+fi
+
 if [ ! "$1" = "--no-edit" ];
 then
   hub pull-request -m "$BRANCH: $1" -m "$2" -b master
